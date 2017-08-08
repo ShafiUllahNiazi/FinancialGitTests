acb226f51373b4e3f09cc7db7348c1327572dd13
ShafiUllahNiazi <shafiullahkhan117786@gmail.com>
2017-08-08 14:22:57 +0500

    some updates after demo

---
+++ transport.php
@@ -0,0 +1,39 @@
+<DIV class="product-item float-clear" >
+<div class="container col-sm-12" >
+	<DIV class="col-sm-2 "></DIV>
+
+
+</div>
+
+
+<div class="container col-sm-4">
+		<DIV class="col-sm-12 ">Transport Type (Car/ Motor cycle/ Others*)<span id="tType-error" class="signup-error"></span></DIV>
+		<DIV class="float-left"><input type="text" name="TransportType[]" id="tType" class="form-control"/></DIV>
+</div>
+
+<div class="container col-sm-4">
+		<DIV class="col-sm-8 ">Make /Model</DIV><span id="tmodel-error" class="signup-error"></span>
+		<DIV class="float-left"><input type="text" name="Model[]" id="tmodel" class="form-control"/></DIV>
+</div>
+
+<div class="container col-sm-4">
+		<DIV class="col-sm-8 ">Engine Capacity (CC)</DIV><span id="tCC-error" class="signup-error"></span>
+		<DIV class="float-left"><input type="text" name="EngineCapacity[]" id="tCC" class="form-control"/></DIV>
+</div>
+<div class="container col-sm-4">
+		<DIV class="col-sm-8 ">Registration No.</DIV><span id="tReg-error" class="signup-error"></span>
+		<DIV class="float-left"><input type="text" name="RegNo[]" id="tReg" class="form-control"/></DIV>
+</div>
+<div class="container col-sm-4">
+		<DIV class="col-sm-8 ">Ownership Period</DIV><span id="townP-error" class="signup-error"></span>
+		<DIV class="float-left"><input type="text" name="OwnPeriod[]" id="townP" class="form-control"/></DIV>
+</div>
+
+
+
+<DIV class="float-left"><input type="checkbox" name="item_index[]" class="form-control"/></DIV><br>
+
+
+
+
+</DIV>
